57f3e83188c995f3987109355d6eacf814bc5d0d
sainttobs <sainttobs@gmail.com>
2019-05-17 04:20:01 +0100

    mutations

--- server2.js
+++ server2.js
@@ -8,6 +8,9 @@ var schema = buildSchema(`
 		course(id: Int!): Course
 		courses(topic: String): [Course]
 	}
+	type Mutation {
+		updateCourseTopic(id: Int!, topic: String!) : Course
+	}
 	type Course {
 		id: Int
 		title: String
@@ -61,9 +64,20 @@ var getCourses =function(args) {
 	}
 }
 
+var updateCourseTopic = function({id, topic}){
+	coursesData.map(course => {
+		if (course.id === id) {
+			course.topic = topic;
+			return course;
+		}
+	});
+	return coursesData.filter(course => course.id === id)[0];
+}
+
 var root = {
 	course: getCourse,
-	courses: getCourses
+	courses: getCourses,
+	updateCourseTopic : updateCourseTopic
 };
 
 // Create an express server and a GraphQL endpoint
